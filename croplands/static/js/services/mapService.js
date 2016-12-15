f0f5dc98000c44c4e234109fe8a1bd1a371f323e
croplands-dev <croplands.dev@gmail.com>
2016-12-15 15:08:00 -0700

    Added Canada 30m Cropland Extent Product
    Canada 30m Product was added to the map.  Also added to the legend

--- croplands/static/js/services/mapService.js
+++ croplands/static/js/services/mapService.js
@@ -265,6 +265,22 @@ app.factory('mapService', ['leafletLayers', function (leafletLayers) {
                         {label: 'Croplands', color: '#00FF00'}
                     ]
                 },
+                Canada_30m_L1_v20161215: {
+                    name: 'Canada 30m Cropland Extent Product Nominal 2010',
+                    visible: true,
+                    type: 'xyz',
+                    params: {
+                        options: {
+                            band: 'class',
+                            subdomains: 'abc',
+                            bounds: L.latLngBounds(L.latLng(33.682697, -121.603351), L.latLng(6.112637, -53.773761))
+                        },
+                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&band={band}&id=Canada_30m_L1_v20161215'
+                    },
+                    legend: [
+                        {label: 'Croplands', color: '#00FF00'}
+                    ]
+                },
                 Africa_250m_L2_v20160601: {
                     name: 'Africa GCE 250m Cropland Products 2003 to 2014 from ACCA',
                     visible: false,

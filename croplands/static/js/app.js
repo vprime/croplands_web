99a9782a7b9f7441ddc549b919f02801d82cbfd0
croplands-dev <croplands.dev@gmail.com>
2017-03-02 14:18:21 -0700

    Fixed Street View Application

--- croplands/static/js/app.js
+++ croplands/static/js/app.js
@@ -2740,6 +2740,46 @@ app.controller("MapController", ['$scope', 'mapService', 'DataService', 'leaflet
 
 }])
 ;;
+/**
+ * Created by Corryn Smith on 2/22/2017.
+ */
+
+app.controller('MarkersSimpleController', [ '$scope', function($scope) {
+            var mainMarker = {
+                lat: 0,
+                lng: 0,
+                focus: true,
+                message: "Hey, drag me if you want",
+                draggable: true
+            };
+
+            angular.extend($scope, {
+                world: {
+                    lat: 0,
+                    lng: 0,
+                    zoom: 1
+                },
+                markers: {
+                    mainMarker: angular.copy(mainMarker)
+                },
+                position: {
+                    lat: 0,
+                    lng: 0
+                },
+                events: { // or just {} //all events
+                    markers:{
+                      enable: [ 'dragend' ]
+                      //logic: 'emit'
+                    }
+                }
+            });
+
+            $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
+                $scope.position.lat = args.model.lat;
+                $scope.position.lng = args.model.lng;
+            });
+
+        } ]);;
 app.controller("NavbarController", ['$scope', 'User', '$location', function ($scope, User, $location) {
     $scope.goToLogin = function () {
         var n = encodeURIComponent(window.btoa(JSON.stringify({
@@ -2947,7 +2987,8 @@ app.controller("StreetController", ['$scope', 'mapService', 'mappings', '$http',
 
     initMap();
 
-}]);;
+}]);
+;
 app.controller("ConfirmController", ['$scope', 'log', function ($scope, log) {
 }]);;
 app.controller("ForgotController", ['$scope', 'User', function ($scope, User) {

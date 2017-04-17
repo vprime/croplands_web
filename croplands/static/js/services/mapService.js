d1b61a80eae68758bd0f8e8d5d470142cd354213
croplands-dev <croplands.dev@gmail.com>
2017-04-16 19:12:27 -0700

    Europe 30m Update

--- croplands/static/js/services/mapService.js
+++ croplands/static/js/services/mapService.js
@@ -259,7 +259,7 @@ app.factory('mapService', ['leafletLayers', function (leafletLayers) {
                             band: 'class',
                             subdomains: 'abc'
                         },
-                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=Europe_30m_L1_v20170403&band={band}'
+                        url: '//{s}.tiles.croplands.org/{z}/{x}/{y}/tile.png?collection=users/croplandsdev/products&id=Europe_30m_L1_v20170416&band={band}'
                     },
                     legend: [
                         {label: 'Cropland', color: '#00FF00'}
